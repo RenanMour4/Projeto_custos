207e1b0a04cda067ad96b268fd9004ca930b2535
RenanMour4 <renan.vanderlei@hotmail.com>
2021-11-12 12:36:20 -0300

    Primeira Versão do Projeto

---
+++ src/components/pages/NovoProjeto.js
@@ -0,0 +1,6 @@
+function NovoProjeto(){
+    return <h1>Página Novo Projeto</h1>
+}
+
+
+export default NovoProjeto
---
+++ src/components/pages/Sobre.js
@@ -0,0 +1,6 @@
+function Sobre(){
+    return <h1>Página Sobre</h1>
+}
+
+
+export default Sobre
