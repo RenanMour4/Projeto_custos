f48647e422a79c677109a2bb2fa9e84ac7ddaf47
RenanMour4 <64485870+RenanMour4@users.noreply.github.com>
2021-11-26 12:32:06 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-
-import Home from './components/pages/Home';
-import Sobre from './components/pages/Sobre';
-import Contato from './components/pages/Contato';
-import NovoProjeto from './components/pages/NovoProjeto';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+// Importando as páginas
+import Home from './components/pages/Home'
+import Sobre from './components/pages/Sobre'
+import Contato from './components/pages/Contato'
+import NovoProjeto from './components/pages/NovoProjeto'
 
 function App() {
   return (
     <Router>
       <div>
         <Link to="/">Home</Link>
-        <Link to="/contato">Contato</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/novoprojeto">Novo Projeto</Link>
+        <Link to="/Sobre">Contato</Link>
+        <Link to="/Contato">Sobre</Link>
+        <Link to="/NovoProjeto">Novo Projeto</Link>
       </div>
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
-        <Route exact path="/sobre">
+        <Route exact path="/Sobre">
           <Sobre />
         </Route>
-        <Route exact path="/contato">
+        <Route exact path="/Contato">
           <Contato />
         </Route>
-        <Route exact path="/novoprojeto">
+        <Route exact path="/NovoProjeto">
           <NovoProjeto />
         </Route>
       </Switch>
       <p>Footer</p>
     </Router>
-  )
+  );
 }
 
 export default App;
